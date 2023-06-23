aa27a7c780fdc7150e62e4aa00bbfeb30af98df8
vadym4che <vadym4che@gmail.com>
2023-06-23 15:49:18 +0300

    add font, ico, logo, basic colors

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="header">
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="header-link"
+          href="/"
         >
-          Learn React
+          <img src={logo} className="header-link-image" alt="logo" />
         </a>
       </header>
     </div>
