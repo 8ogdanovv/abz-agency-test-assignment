e59b9a0f026bd05a8031a6da4e35a0fcba65a1de
vadym4che <vadym4che@gmail.com>
2023-07-02 15:26:58 +0300

    Updates

---
+++ static/js/156.c4bed933.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkabz_agency_test_assignment=self.webpackChunkabz_agency_test_assignment||[]).push([[156],{156:function(s,e,c){c.r(e),c.d(e,{default:function(){return n}});c(791);var t=c(184),n=function(){return(0,t.jsxs)("div",{className:"section success block",id:"sign-up-page",children:[(0,t.jsx)("p",{className:"text-title",children:"User successfully registered"}),(0,t.jsx)("div",{className:"success-picture"})]})}}}]);
+//# sourceMappingURL=156.c4bed933.chunk.js.map
